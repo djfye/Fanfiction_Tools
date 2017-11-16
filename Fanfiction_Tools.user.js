6984160423935ac87a18f1baaf1260db1b5965b1
djfye <djfye2002@yahoo.com>
2017-11-15 21:17:18 -0600

    updateVersion

--- Fanfiction_Tools.user.js
+++ Fanfiction_Tools.user.js
@@ -13,6 +13,7 @@
 // @resource      miniColorsCss https://cdnjs.cloudflare.com/ajax/libs/jquery-minicolors/2.2.6/jquery.minicolors.css
 // @updateURL     https://github.com/djfye/Fanfiction_Tools/raw/master/Fanfiction_Tools.meta.js
 // @downloadURL   https://github.com/djfye/Fanfiction_Tools/raw/master/Fanfiction_Tools.user.js
+// @updateVersion 5
 // @grant         GM_getValue
 // @grant         GM_setValue
 // @grant         GM_addStyle
