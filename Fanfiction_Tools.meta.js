3eb05ed2a697ef7985d8e52e892cb83bd6dcd5fd
djfye <djfye2002@yahoo.com>
2023-06-19 02:56:41 -0500

    Delete Fanfiction_Tools.meta.js

--- Fanfiction_Tools.meta.js
+++
@@ -1,9 +0,0 @@
-﻿// ==UserScript==
-// @name          Fanfiction Tools
-// @author        Ewino
-// @version       1.7.5
-// @description   Enhances fanfiction.net.
-// @namespace     http://userscripts.org/scripts/show/102342
-// @updateURL     https://github.com/djfye/Fanfiction_Tools/raw/master/Fanfiction_Tools.meta.js
-// @downloadURL   https://github.com/djfye/Fanfiction_Tools/raw/master/Fanfiction_Tools.user.js
-// ==/UserScript==
